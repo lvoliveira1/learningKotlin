e717deee1f487900d213d94f6589dae0ae207830
Luciano <luciano@Apples-MacBook-Pro.local>
2018-04-04 19:06:44 -0300

    testing it with Adam, Thank you Adam

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,13 @@
 // This is main, the entry point of a Java/Kotlin Application
 fun main(args: Array<String>) {
-    TODO("Run Assignment functions here")
+
+    val myBook = Student(title = "The lord of the rings", author = "Tolkien", pages = 12345)
+
+    println(myBook.getDetails())
+    
+    myBook.price = 45.9
+    println(myBook.price)
+
 }
+
+
--- src/Student.kt
+++ src/Student.kt
@@ -1,2 +1,20 @@
+
+// Title = String
+// Numbers = Integer
+
 class Student {
+    val title: String
+    val author: String
+    private val pages: Int
+    var price = 0.0
+
+    constructor(title: String, author: String, pages: Int){
+        this.title = title
+        this.author = author
+        this.pages = pages
+    }
+    fun getDetails(): String {
+        return  "Title: $title, Author: $author, Pages: $pages"
+    }
+
 }
