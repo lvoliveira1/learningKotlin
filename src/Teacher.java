00980cdac55f70613b2081eecc17eba9cbf80512
Luciano Valente de Oliveira <lvoliveira1@gmail.com>
2018-04-05 16:59:17 -0300

    Teacher.

---
+++ src/Teacher.java
@@ -0,0 +1,22 @@
+class Teacher {
+    val firstName:String
+    val lastName:String
+    val Age:Integer
+    val gender:String
+    val currentSubject:
+
+    constructor(firstName:String, lastName:String, Age:Integer, gender:String, currentSubject:String) {
+        this.firstName = FirstName
+        this.lastName = LastName
+        this.Age = age
+        this.gender = Gender
+        this.currentSubject = CurrentSubject
+        }
+
+    fun getDetailsTeacher():String
+
+    {
+        return "First Name: $FirstName, Last Name: $LastName, Age: $age, Gender= $Gender, Cur. Subj., $CurrentSubject"
+    }
+
+}
